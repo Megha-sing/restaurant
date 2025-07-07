41b52ca7a67a0f78786505caf12c47f31a6070da
Megha Singh <162406163+Megha-sing@users.noreply.github.com>
2025-07-07 16:47:57 +0530

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -22,7 +22,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "http://restaurant-5siv.onrender.com/api/v1/reservation/send",
         {
           firstName,
           lastName,
